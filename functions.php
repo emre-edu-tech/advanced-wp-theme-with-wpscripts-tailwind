fb881c801f68f33c3e927abf6321aa6b92864d8d
Emre Kayikcilar <emreebrukayikcilar@gmail.com>
2024-05-09 21:59:15 +0300

    Custom post type Topic and custom taxonomy Forum have been added

--- functions.php
+++ functions.php
@@ -25,5 +25,73 @@ function mponsportal_theme_support() {
     // Support for custom logo upload
     add_theme_support('custom-logo');
     // Support for overriding woocommeerce templates in this custom theme
-    add_theme_support('woocommerce');
+    // thumbnail-image_width, single_image_width can be controlled: 
+    // Customizer > Woocommerce > Product Images
+    // product_grid settings can be controlled:
+    // Customizer > Woocommerce > Product Catalog
+    add_theme_support('woocommerce', [
+        'thumbnail_image_width' => 150,
+        'single_image_width' => 600,
+        'product_grid' => [
+            'default_rows' => 3,
+            'default_columns' => 4,
+        ]
+    ]);
+    // Support for Navigation Menus
+    register_nav_menus([
+        'primary-menu' => 'Primary Menu',
+        'footer-menu' => 'Footer Menu'
+    ]);
+}
+
+add_action('init', 'mponsportal_custom_post_types_taxonomies');
+function mponsportal_custom_post_types_taxonomies() {
+    // Note: There is no "archives" or "view items" labels in $topic_args array 
+    // because Topic custom post type has_archive set to false
+    $topic_args = [
+        'name' => _x('Topics', 'Post Type Plural Name', 'mponsportal'),
+        'singular_name' => _x('Topic', 'Post Type Singular Name', 'mponsportal'),
+        'menu_name' => __('Topics', 'mponsportal'),
+        'attributes' => __('Topic Attributes', 'mponsportal'),
+        'all_items' => __('All Topics', 'mponsportal'),
+        // Page title for the Add New Post Page
+        'add_new_item' => __('Add New Topic', 'mponsportal'),
+        // Add New Post Menu Text on admin bar and Add New Button Label on All Topics Page
+        'add_new' => __('Add New Topic', 'mponsportal'),
+        'new_item' => __('New Topic', 'mponsportal'),
+        'edit_item' => __('Edit Topic', 'mponsportal'),
+        'view_item' => __('View Topic', 'mponsportal'),
+        'search_items' => __('Search Topics', 'mponsportal'),
+        // not_found label is for the All Topics page if there all the topics are deleted
+        'not_found' => __('No topics found', 'mponsportal'),
+        'not_found_in_trash' => __('No topics found in Trash', 'mponsportal'),
+        // Because topic post type has no featured-image in its supports setting
+        // There is no need to set the featured-image related labels here
+    ];
+    // Custom Post Type: Topic
+    // We do not want any archive page for this post type so 'has_archive' argument is not set to true
+    register_post_type('topic', [
+        'label' => __('Topics', 'mponsportal'),
+        'labels' => $topic_args,
+        'show_ui' => true,  // this also determines the value of 'show_in_menu' and 'show_in_admin_bar'
+        'supports' => ['title', 'editor', 'excerpt', 'custom-fields'],
+        'show_in_rest' => true,
+        'menu_icon' => 'dashicons-media-document',
+        'taxonomies' => ['forum'],
+        'public' => true,
+        // post type is not available for selection in navigation menus, default to value of 'public' argument
+        'show_in_nav_menus' => false,
+    ]);
+
+    // Custom Taxonomy - Forum: related to Topic
+    register_taxonomy('forum', ['topic'], [
+        'label' => __('Forums', 'mponsportal'),
+        'show_ui' => true,
+        'show_admin_column' => true,
+        'show_in_nav_menus' => true,
+        'show_in_rest' => true,
+        'hierarchical' => true,
+    ]);
+    // Guarantee the relationship between post type and taxonomy
+    register_taxonomy_for_object_type('forum', 'topic');
 }
